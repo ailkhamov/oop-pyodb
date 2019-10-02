76cea07729c177e0969268f94883a4e130cf8538
Abror Ilkhamov <ailkhamov@spartaglobal.com>
2019-10-02 11:22:07 +0100

    Basic manual connection done

--- dbconnect.py
+++ dbconnect.py
@@ -19,21 +19,21 @@ print(conn_northwind)
 # Allows us to execute readonly queries of our database
 cursor = conn_northwind.cursor()
 
-cursor.execute("SELECT * FROM Customers;")
-# Fetch rows from cursor - Fetch one
-row = cursor.fetchone()
-
-print(row)
-# Fetch all
-# This is bad MM'kay
-# We dont' you this mm'kay...
-rows = cursor.execute("SELECT * FROM Customers;").fetchall()
-for row in rows:
-    print(row)
+# cursor.execute("SELECT * FROM Customers;")
+# # Fetch rows from cursor - Fetch one
+# row = cursor.fetchone()
+#
+# print(row)
+# # Fetch all
+# # This is bad MM'kay
+# # We dont' you this mm'kay...
+# rows = cursor.execute("SELECT * FROM Customers;").fetchall()
+# for row in rows:
+#     print(row)
 
 # Fetch some data using for loops
-rows = cursor.execute("SELECT * FROM Products").fetchall()
-count = 0
+# rows = cursor.execute("SELECT * FROM Products").fetchall()
+# count = 0
 #for row in rows:
   #  print(row.UnitPrice) # We can access the column of a specific record
 
@@ -41,10 +41,22 @@ count = 0
 # Because we can clog our machine with too much date
     # We can use While Loop to be more effiecent
 
-rows = cursor.execute('SELECT *  FROM Products')
+rows = cursor.execute('SELECT *  FROM Orders').fetchall()
+print("Question1")
+print(len(rows))
+# while True:
+#     record = rows.fetchone()
+#     if record is None:
+#         break
+#     print(len(record))
 
-while True:
-    record = rows.fetchone()
-    if record is None:
-        break
-    print(record.UnitPrice)
+print("Question2")
+question2 = cursor.execute("SELECT * FROM Orders WHERE ShipCity = 'Rio de Janeiro'").fetchall()
+print(len(question2))
+
+print("Question3")
+question3 = cursor.execute("SELECT * FROM Orders WHERE ShipCity = 'Rio de Janeiro' OR ShipCity = 'Reims'").fetchall()
+for row in question3:
+    print(row)
+print("Number of orders for Question 3")
+print(len(row))
